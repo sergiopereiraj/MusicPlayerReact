1dbce228eb0ad40b20c1a061295109a628eb4602
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-12-22 21:12:45 +0000

    guardado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [sing, setSing] = useState([]);
+	const [url] = useState("https://assets.breatheco.de/apis/sound/songs");
+	const [condicion, setCondicion] = useState(false);
+	const [song, setSong] = useState([]);
+
 	useEffect(() => {
 		obtenerDatos();
 	}, []);
+	let audioRef = useRef(null);
+
+	const reproducir = url => {
+		if (
+			audioRef.current.src ===
+			"https://assets.breatheco.de/apis/sound/" + url
+		) {
+			audioRef.current.play();
+		} else {
+			audioRef.current.src =
+				"https://assets.breatheco.de/apis/sound/" + url;
+			audioRef.current.play();
+		}
+		setCondicion(true);
+	};
 
 	const obtenerDatos = async () => {
-		const data = await fetch("https://assets.breatheco.de/apis/sound/fx");
+		const data = await fetch(url);
 		const cancion = await data.json();
-		setSing(cancion);
+		setSong(cancion);
 	};
 
 	return (
@@ -24,14 +42,21 @@ const Home = () => {
 						Winamp 60.60vs <i className="fas fa-music"></i>
 					</h1>
 				</header>
-				<ol>
-					{sing.map(item => (
-						<li key={item.id}>
-							{item.name} - {item.category}
+				<div id="botones" className="">
+					{song.map(item => (
+						<li key={item.id} className="d-grid gap-2">
+							<button
+								type="button"
+								className="btn btn-secondary p-2 bd-highlight"
+								onClick={() => {
+									reproducir(song.url);
+								}}>
+								{item.name} -
+							</button>
 						</li>
 					))}
-				</ol>
-				<footer className="fixed-bottom">
+				</div>
+				<footer className="">
 					<button id="previous">
 						<i className="fas fa-caret-square-left"></i>
 					</button>
